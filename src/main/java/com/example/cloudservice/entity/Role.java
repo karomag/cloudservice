26b26dd5a37140a0ab50d35f90857af19a4e234f
karomag <karomag88@mail.ru>
2024-02-05 20:25:22 +0300

    new way to do SS

--- src/main/java/com/example/cloudservice/entity/Role.java
+++ src/main/java/com/example/cloudservice/entity/Role.java
@@ -6,28 +6,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
-import java.util.Collection;
-import java.util.List;
-
 @Entity
 @Data
 @NoArgsConstructor
 @Accessors(chain = true)
 @AllArgsConstructor
-@Table(name = "users")
-public class User {
-
+@Table(name = "roles")
+public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(unique = true, nullable = false)
-    private String username;
-
-    @Column(nullable = false)
-    private String password;
-
-   @ManyToMany(fetch = FetchType.LAZY)
-    private Collection<Role> roles = List.of();
-
+    private String name;
 }
